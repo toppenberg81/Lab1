452b7c6b400f7194811ca3c3210dedb71d56668e
scrypy <scrypy@localhost.localdomain>
2018-01-27 17:45:10 -0700

    init lab1 starter

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <cstdlib>
+#include <iostream>
+//include other headers here
+
+
+using namespace std;
+int main(int argc, char *argv[]){
+  //use argv to get the file name
+
+  //TODO: Open the provided file and check if it failed
+
+  //TODO: Get number count
+  //TODO: Get first & last 2 numbers
+
+  //TODO: Close the file and report results
+
+}
