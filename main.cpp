6449934755738a722e5cecd4bbf7b2f69f0da67b
toppenberg81 <35778958+toppenberg81@users.noreply.github.com>
2018-02-08 12:18:36 -0700

    dug a new hole
    remember to change debugging properties

--- main.cpp
+++ main.cpp
@@ -4,48 +4,45 @@
 //January 29, 2018
 
 
+
 #include <cstdlib>
 #include <iostream>
-#include <string>
-#include <fstream>
+#include<fstream>
 
-using std::string;
-using std::ifstream;
+
 //include other headers here
 
-
+using std::ifstream;
 using namespace std;
 int main(int argc, char *argv[]) {
+
 	ifstream files;
-	string filename;
-	//use argv to get the file name
-	cout << "Please enter the file name: \n";
-	//cin >> *argv;
-	getline(cin, filename);
-	cout << filename;
-	files.open(filename);	
-	if (files.is_open())
-	{
-		cout << "did it";
-	}
-	//TODO: Open the provided file and check if it failed
-
-	files.open(filename);
-	if (files.is_open())
-	{
-		cout << "did it";
-	}
 	files.open(*argv);
-	cout <<"opened file"<< endl;
-
-	while (!files);
+	cout << argc;
+	if (files.is_open())
 	{
-		cout << "Could not open: " << endl;
+		cout << *argv<<"\n";//printing out executible location
+		
 	}
+
+	
+	while (files.good())//does nothing
+
+	{
+		files >> argc;
+		//only prints 2's for all 3 files!?
+		cout << argc << "\n";
+	}
+	//use argv to get the file name
+	//*check if argv has been initalized ELSE pull in with string (don't need..)
+	//TODO: Open the provided file and check if it failed
 
 	//TODO: Get number count
 	//TODO: Get first & last 2 numbers
 
 	//TODO: Close the file and report results
-	cout << "end of prog";
+	cout << "end of prog\n";
 }
+
+//help Danny Radosevich 
+//lots of cplusplus.com forums
