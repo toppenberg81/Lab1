5653a9b90da773dffe17da67183ca2061838f45b
toppenberg81 <35778958+toppenberg81@users.noreply.github.com>
2018-02-09 13:00:21 -0700

    final version
    Got help from cplusplus.com and a friend, Danny Radosevich

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@
 #include <cstdlib>
 #include <iostream>
 #include<fstream>
+#include<string>
 
 
 //include other headers here
@@ -17,31 +18,62 @@ using namespace std;
 int main(int argc, char *argv[]) {
 
 	ifstream files;
+	string filename; 
+	cout << "Please enter your file name: \n";
+	cin >> filename;
+	char *string = new char[filename.length() + 1];
+	strcpy(string, filename.c_str());
+	*argv = string;
+
+
+	//*argv = "num1.txt"; I tried alot of different things and
+	//could not get the command line input to work through 
+	//Visual studios I found the debugging tab and put in 
+	//the executable location followed by the file name with no success
+
+
 	files.open(*argv);
-	cout << argc;
+	
+	int frst_count = 0;
+	cout << "\nThe first two nubmers are: ";
+	while (!files.fail())
+	{
+		while (files >> argc)
+		{
+			if (frst_count == 0 || frst_count == 1)
+			{
+				cout <<"\n "<< argc<<"\n";
+			}
+			frst_count++;
+		}
+	}
+	cout << "\nThere are " << frst_count << " integers in the file.\n ";
+	files.close();
+	files.open(*argv);
+
+	cout << "\nThe last two numbers are: ";
+
+	int snd_count = 0;
 	if (files.is_open())
 	{
-		cout << *argv<<"\n";//printing out executible location
-		
+		while (files >> argc)
+		{
+			snd_count++;
+			if (snd_count == frst_count - 1 || snd_count == frst_count)
+			{
+				cout << "\n" << argc<<"\n";
+			}
+		}
 	}
 
+	files.close();
 	
-	while (files.good())//does nothing
-
-	{
-		files >> argc;
-		//only prints 2's for all 3 files!?
-		cout << argc << "\n";
-	}
-	//use argv to get the file name
-	//*check if argv has been initalized ELSE pull in with string (don't need..)
-	//TODO: Open the provided file and check if it failed
 
 	//TODO: Get number count
 	//TODO: Get first & last 2 numbers
 
 	//TODO: Close the file and report results
-	cout << "end of prog\n";
+	cout << "\nend of prog\n";
 }
 
 //help Danny Radosevich 
